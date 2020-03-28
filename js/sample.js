9422e32e9f85bafc532e2001a5e001aecdf7ca30
Ravin Kumar <ravinsdrive@gmail.com>
2020-03-28 09:44:34 -0700

    Add basics

---
+++ js/sample.js
@@ -0,0 +1,11 @@
+"use strict";
+function greeter(person) {
+    /**
+     * @person {string} Name of user
+     *
+     */
+    return "Hello, " + person;
+}
+var user = "ravin";
+// document.body.innerHTML = greeter(user);
+console.log(greeter(user));
---
+++ js/sample.ts
@@ -0,0 +1,12 @@
+function greeter(person: string) {
+    /**
+     * @person {string} Name of user
+     * 
+     */
+    return "Hello, " + person;
+}
+
+var user = "ravin";
+
+// document.body.innerHTML = greeter(user);
+console.log(greeter(user));
